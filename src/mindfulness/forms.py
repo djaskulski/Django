4ae3666d7bd0f80cd0ddbbef3780e47f2067ca65
Dawid Jaskulski <djaskulski@edu.cdv.pl>
2021-03-17 10:04:31 +0100

    initial_values

--- src/mindfulness/forms.py
+++ src/mindfulness/forms.py
@@ -3,6 +3,20 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(label='TITLE', widget=forms.TextInput(attrs={"placeholder": "hi you"}))
+    description = forms.CharField(required=True,
+                                  widget=forms.Textarea(
+                                      attrs={
+                                          "placeholder": "say my name",
+                                          "class": "new-class-name",
+                                          "rows": 15,
+                                          "cols": 20
+                                      }
+                                  )
+                                  )
+    email = forms.EmailField()
+    price = forms.DecimalField(initial=199.99)
+
     class Meta:
         model = Product
         fields = [
@@ -10,10 +24,35 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get('title')
+        if not "gold" in title:
+            raise forms.ValidationError("This is not a gold product")
+        if not "silver" in title:
+            raise forms.ValidationError("This is not a silver product")
+        return title
+
+    def clean_description(self, *args, **kwargs):
+        description = self.cleaned_data.get('description')
+        if not "Producted in" in description:
+            raise forms.ValidationError("This is not 'Producted in")
+        if not "Composition" in description:
+            raise forms.ValidationError("This is not 'Composition'")
+        return description
+
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get('email')
+        if not email.endswith(".com"):
+            raise forms.ValidationError("Do not forgot .com")
+        if not "edu" in email:
+            raise forms.ValidationError("Please use your student's email")
+        return email
+
+
 
 
 class RawProductForm(forms.Form):
-    title = forms.CharField(label=' ', widget=forms.TextInput(attrs={"placeholder": "hi you"}))
+    title = forms.CharField(label='TITLE', widget=forms.TextInput(attrs={"placeholder": "hi you"}))
     description = forms.CharField(required=True,
                                   widget=forms.Textarea(
                                       attrs={
--- src/mindfulness/views.py
+++ src/mindfulness/views.py
@@ -16,6 +16,22 @@ def product_detail_view(request):
     return render(request, "products/product_detail.html", context)
 
 
+def product_create_view(request):
+    initial_data = {
+        'title': "Gold title of your choice",
+        'description': "Producted in: Composition:",
+        'email': "your_mail@edu.com"
+    }
+    form = ProductForm(request.POST or None, initial=initial_data)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, "products/product_create.html", context)
+
 # def product_create_view(request):
 #     form = ProductForm(request.POST or None)
 #     if form.is_valid():
@@ -39,18 +55,18 @@ def product_detail_view(request):
 #     return render(request, "products/product_create.html", context)
 
 
-def product_create_view(request):
-    my_form = RawProductForm()
-    if request.method == "POST":
-        my_form = RawProductForm(request.POST)
-        if my_form.is_valid():
-            print(my_form.cleaned_data)
-            Product.objects.create(**my_form.cleaned_data)
-        else:
-            print(my_form.errors)
-    context = {
-        "form": my_form
-    }
-    return render(request, "products/product_create.html", context)
+# def product_create_view(request):
+#     my_form = RawProductForm()
+#     if request.method == "POST":
+#         my_form = RawProductForm(request.POST)
+#         if my_form.is_valid():
+#             print(my_form.cleaned_data)
+#             Product.objects.create(**my_form.cleaned_data)
+#         else:
+#             print(my_form.errors)
+#     context = {
+#         "form": my_form
+#     }
+#     return render(request, "products/product_create.html", context)
 
 
