1b670ffcec81a6e3b67dd252858f38d11e925cbb
Dawid Jaskulski <djaskulski@edu.cdv.pl>
2021-03-21 12:28:38 +0100

    initial_values3

--- src/Django_project/urls.py
+++ src/Django_project/urls.py
@@ -9,7 +9,7 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+Including another URL conf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
@@ -24,30 +24,27 @@ from pages.views import (
     yoga_view,
     contact_view,
     about_view
-    )
+)
 from mindfulness.views import (
     product_detail_view,
     product_create_view,
     product_delete_view,
     product_list_view,
-    dynamic_lookup_view
-    )
+)
 
 urlpatterns = [
     path('', about_view, name='about'),
 
-    path('mindfulness/', mindfulness_view, name='mindfulness'),
-    path('product/', product_detail_view),
-    path('products/', product_list_view),
-    path('products/<int:id>/', dynamic_lookup_view),
+    path('products/', product_list_view, name='product-list'),
+    path('products/<int:id>/', product_detail_view, name='product-detail'),
     path('products/<int:id>/delete/', product_delete_view, name='product-delete'),
-    path('create/', product_create_view),
+    path('create/', product_create_view, name='product-create'),
 
+    path('mindfulness/', mindfulness_view, name='mindfulness'),
     path('minimalism/', minimalism_view, name='minimalism'),
     path('recycling/', recycling_view, name='recycling'),
     path('vegetarianism/', vegetarianism_view, name='vegetarianism'),
     path('yoga/', yoga_view, name='yoga'),
     path('contact/', contact_view, name='contact'),
-
     path('admin/', admin.site.urls),
 ]
--- src/mindfulness/views.py
+++ src/mindfulness/views.py
@@ -1,10 +1,10 @@
-from django.http import Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 from .forms import ProductForm, RawProductForm
 
+
 # Create your views here.
-def dynamic_lookup_view(request, id):
+def product_detail_view(request, id):
     obj = get_object_or_404(Product, id=id)
     # try:
     #     obj = Product.objects.get(id=id)
@@ -17,24 +17,11 @@ def dynamic_lookup_view(request, id):
 
 
 def product_list_view(request):
-    queryset = Product.objects.all() # list of objects
+    queryset = Product.objects.all()  # list of objects
     context = {
         'object_list': queryset,
     }
     return render(request, "products/product_list.html", context)
-
-
-def product_detail_view(request):
-    obj = Product.objects.get(id=1)
-    # context = {
-    #     "title": obj.title,
-    #     "description": obj.description,
-    #     "price": obj.price,
-    # }
-    context = {
-        'object': obj,
-    }
-    return render(request, "products/product_detail.html", context)
 
 
 def product_create_view(request):
@@ -64,6 +51,19 @@ def product_delete_view(request, id):
         "object": obj
     }
     return render(request, "products/product_delete.html", context)
+
+# def product_detail_view(request):
+#     obj = Product.objects.get(id=1)
+#     # context = {
+#     #     "title": obj.title,
+#     #     "description": obj.description,
+#     #     "price": obj.price,
+#     # }
+#     context = {
+#         'object': obj,
+#     }
+#     return render(request, "products/product_detail.html", context)
+
 
 # def product_create_view(request):
 #     form = ProductForm(request.POST or None)
@@ -101,5 +101,3 @@ def product_delete_view(request, id):
 #         "form": my_form
 #     }
 #     return render(request, "products/product_create.html", context)
-
-
