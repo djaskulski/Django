7a1e41d2e1b48877c20a90a9be78da25332998c7
Dawid Jaskulski <djaskulski@edu.cdv.pl>
2021-03-26 22:12:09 +0100

    polls_app_commit3_f

--- src/polls/urls.py
+++ src/polls/urls.py
@@ -3,7 +3,9 @@ from .views import (
     PollDetailView,
     PollListView,
     # MyListView,
-    PollCreateView
+    PollCreateView,
+    PollUpdateView,
+    PollDeleteView
     # my_fbv
 )
 
@@ -14,7 +16,7 @@ urlpatterns = [
 
     path('create/', PollCreateView.as_view(), name='polls-create'),
     path('<int:id>/', PollDetailView.as_view(), name='polls-detail'),
-    # path('<int:id>/update/', <>, name='polls-delete'),
-    # path('<int:id>/delete/', <>, name='polls-delete')
+    path('<int:id>/update/', PollUpdateView.as_view(), name='polls-update'),
+    path('<int:id>/delete/', PollDeleteView.as_view(), name='polls-delete')
 
 ]
--- src/polls/views.py
+++ src/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 
 from .models import Poll
@@ -6,6 +6,61 @@ from .forms import PollModelForm
 
 
 # Create your views here.
+class PollObjectMixin(object):
+    model = Poll
+
+    def get_object(self):
+        id = self.kwargs.get('id')
+        obj = None
+        if id is not None:
+            obj = get_object_or_404(self.model, id=id)
+        return obj
+
+
+class PollDeleteView(PollObjectMixin, View):
+    template_name = "polls/poll_delete.html"
+
+    def get(self, request, id=None, *args, **kwargs):
+        context = {}
+        obj = self.get_object()
+        if obj is not None:
+            context['object'] = obj
+        return render(request, self.template_name, context)
+
+    def post(self, request, id=None, *args, **kwargs):
+        context = {}
+        obj = self.get_object()
+        if obj is not None:
+            obj.delete()
+            context['object'] = None
+            return redirect('/polls/')
+        return render(request, self.template_name, context)
+
+
+class PollUpdateView(PollObjectMixin, View):
+    template_name = "polls/poll_update.html"
+
+    def get(self, request, id=None, *args, **kwargs):
+        context = {}
+        obj = self.get_object()
+        if obj is not None:
+            form = PollModelForm(instance=obj)
+            context['object'] = obj
+            context['form'] = form
+        return render(request, self.template_name, context)
+
+    def post(self, request, id=None, *args, **kwargs):
+        context = {}
+        obj = self.get_object()
+        if obj is not None:
+            form = PollModelForm(request.POST, instance=obj)
+            if form.is_valid():
+                form.save()
+            context['object'] = obj
+            context['form'] = form
+        return render(request, self.template_name, context)
+
+
 class PollCreateView(View):
     template_name = "polls/poll_create.html"
 
@@ -35,18 +90,18 @@ class PollListView(View):
         return render(request, self.template_name, context)
 
 
-class MyListView(PollListView):
-    queryset = Poll.objects.filter(id=1)
+# class MyListView(PollListView):
+#     queryset = Poll.objects.filter(id=1)
 
 
-class PollDetailView(View):
+class PollDetailView(PollObjectMixin, View):
     template_name = "polls/poll_detail.html"
 
     def get(self, request, id=None, *args, **kwargs):
-        context = {}
-        if id is not None:
-            obj = get_object_or_404(Poll, id=id)
-            context['object'] = obj
+        context = {'object': self.get_object()}
+        # if id is not None:
+        #     obj = get_object_or_404(Poll, id=id)
+        #     context['object'] = obj
         return render(request, self.template_name, context)
 
     # def post(self, request, *args, **kwargs):
